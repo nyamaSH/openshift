b9ddbe499b1af5e2eecfddda15e77adfcd50daad
shaandar@us.ibm.com <30756123+nyamaSH@users.noreply.github.com>
2020-03-19 20:19:31 -0500

    the 4-th commit

--- myopenshiftexample/src/main/java/com/example/demo/MyopenshiftexampleApplication.java
+++ myopenshiftexample/src/main/java/com/example/demo/MyopenshiftexampleApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 
@@ -12,6 +13,11 @@ public class MyopenshiftexampleApplication {
 	public String welcome()
 	{
 		return "hello";
+	}
+	@GetMapping("/{input}")
+	public String congrats(@PathVariable String input )
+	{
+		return input;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(MyopenshiftexampleApplication.class, args);
